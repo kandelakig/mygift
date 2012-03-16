2f5a2ef53e2d9304afc30ebbc0161195ff4e8cee
Giorgi Kandelaki <kandelakig@gmail.com>
2012-03-17 02:06:34 +0400

    just changed spaces to TABs

--- chrome-ex/app.js
+++ chrome-ex/app.js
@@ -1,15 +1,15 @@
 var db = $.couch.db("test"); // satestod bazas qvia "Test", momavalshi sheicvleba
 
 function sendRequest(tab) {
-  db.saveDoc({ // db.saveDoc agzavnis http requests couchDB-stan da aseivebs JSON documents bazashi
-    "extensionId": getExtensionId(), // es veli uazrod davamate, ar vici rameshi gamogvadgeba tu ara
-    "url": tab.url, // zogadad JSON-is struqtura mosafiqrebelia
-    "created_at": new Date()
-  }, {
-    "success": function(data){
-      stopAnimation(); // gavacheret knopka :)
-      console.log("Request saved with _id=\""+data["id"]+"\""); // serveris pasuxs ubralod valogirebt
-    }
-  });
-  startAnimation(); // daiwyo knopkam triali
+	db.saveDoc({ // db.saveDoc agzavnis http requests couchDB-stan da aseivebs JSON documents bazashi
+		"extensionId": getExtensionId(), // es veli uazrod davamate, ar vici rameshi gamogvadgeba tu ara
+		"url": tab.url, // zogadad JSON-is struqtura mosafiqrebelia
+		"created_at": new Date()
+	}, {
+		"success": function(data){
+			stopAnimation(); // gavacheret knopka :)
+			console.log("Request saved with _id=\""+data["id"]+"\""); // serveris pasuxs ubralod valogirebt
+		}
+	});
+	startAnimation(); // daiwyo knopkam triali
 };
