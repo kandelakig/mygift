dad2c2852f7deb5e280da2850c2349d06ff1affc
Giorgi Kandelaki <kandelakig@gmail.com>
2012-03-16 19:30:51 +0400

    Added 2 more test url-s

--- chrome-ex/history.js
+++ chrome-ex/history.js
@@ -5,8 +5,16 @@ function logAll(data) {
   } );
   return data;
 }
-function test() {
+function test1() {
 	var url = "http://www.amazon.com/Waring-WHM100-Professional-10-Speed-Mixer/dp/B0036FRRR0/ref=sr_1_1?s=kitchen&ie=UTF8&qid=1330270558&sr=1-1";
+	return retreiveImages(url).then(logAll);
+}
+function test2() {
+	var url = "http://www.thinkgeek.com/tshirts-apparel/jewelry/e9b7/?pfm=homepage_Featured_3_e9b7";
+	return retreiveImages(url).then(logAll);
+}
+function test3() {
+	var url = "http://www.fabuloustreet.com/index.php?main_page=product_info&cPath=1_93&products_id=1064";
 	return retreiveImages(url).then(logAll);
 }
 
